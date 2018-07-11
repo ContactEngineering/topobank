aa50e1be7507d3a59d366d6548b8c5f6aef99f3d
Michael Röttger <roettger@tf.uni-freiburg.de>
2018-07-11 16:59:09 +0200

    Added test for listing two topographies.
    This test uses a database fixture file, so
    we can easily have a predefined database with
    user and topographies (not uploaded yet in fixture).

--- topobank/manager/tests/test_handling.py
+++ topobank/manager/tests/test_handling.py
@@ -1,24 +1,40 @@
+
+import pytest
 from django.shortcuts import reverse
+from django.core.management import call_command
+
 from pathlib import Path
 import datetime
-import pytest
 
 from ..models import Topography
+from topobank.users.models import User
 
 def export_reponse_as_html(response, fname='/tmp/response.html'):
     f = open(fname, mode='w')
     f.write(str(response.content).replace('\\n','\n'))
     f.close()
 
+
+@pytest.fixture(scope='session')
+def two_topos(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', 'two_topographies.yaml')
+
+        # Fix the passwords of fixtures
+        for user in User.objects.all():
+            user.set_password(user.password)
+            user.save()
+
+        # like this we can have clear-text passwords in test fixtures
+
 #
 # Different formats are handled by PyCo
 # and should be tested there
 #
-
 @pytest.mark.django_db
 def test_upload_topography(client, django_user_model):
 
-    # input_file_path = Path('../PyCo-web/PyCo_app/data/gain_control_uncd_dlc_4.004')
+    # input_file_path = Path('../../../PyCo-web/PyCo_app/data/gain_control_uncd_dlc_4.004')
     input_file_path = Path('../fixtures/example4.txt') # TODO use standardized way to find files
     description = "test description"
 
@@ -60,6 +76,32 @@ def test_upload_topography(client, django_user_model):
     #
     response = client.get(reverse('manager:list'))
     assert bytes(description, 'utf-8') in response.content
+
+
+@pytest.mark.django_db
+def test_topography_list(client, two_topos, django_user_model):
+
+    user = django_user_model.objects.get(username='testuser')
+
+    assert client.login(username=user.username, password='abcd$1234')
+
+    #
+    # all topographies for 'testuser' should be listed
+    #
+    topos = Topography.objects.filter(user__username=user.username)
+    response = client.get(reverse('manager:list'))
+
+    content = str(response.content)
+    for t in topos:
+        # currently 'listed' means: filename and description in list
+        assert t.datafile.name in content
+        assert t.description in content
+
+        # click on a row should lead to details, so URL must be included
+        assert reverse('manager:detail', kwargs=dict(pk=t.pk)) in content
+
+        # TODO real test for click should be done with selenium
+
 
 
 
