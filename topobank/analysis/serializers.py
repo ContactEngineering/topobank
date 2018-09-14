7aa866b47863b8fb2e4a66f551fd6c18054c6d7a
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2018-09-14 17:45:31 +0200

    ENH: Return result dictionary as JSON

--- topobank/analysis/serializers.py
+++ topobank/analysis/serializers.py
@@ -1,9 +1,18 @@
+import pickle
+
 from rest_framework import serializers
 
 from .models import Analysis
 
 
+class PickledResult(serializers.DictField):
+    def to_representation(self, value):
+        return super().to_representation(pickle.loads(value))
+
+
 class AnalysisSerializer(serializers.ModelSerializer):
+    result = PickledResult()
+
     class Meta:
         model = Analysis
-        fields = ('id', 'task_id', 'task_state')
+        fields = ('id', 'task_id', 'task_state', 'result')
