ENH: Gaussian also for slope distribution BUG: Mean of bin computation was wrong

topobank/analysis/functions.py
@@ -18,7 +18,7 @@ def height_distribution(topography, bins=None, wfac=5):
     mean_height = np.mean(profile)
     rms_height = topography.compute_rms_height()
 
-    hist, bin_edges = np.histogram(np.ma.compressed(profile), bins=bins, normed=True)
+    hist, bin_edges = np.histogram(np.ma.compressed(profile), bins=bins, density=True)
 
     minval = mean_height - wfac * rms_height
     maxval = mean_height + wfac * rms_height
@@ -35,7 +35,7 @@ def height_distribution(topography, bins=None, wfac=5):
         ylabel='Probability ({}⁻¹)'.format(topography.unit),
         series=[
             dict(name='Height distribution',
-                 x=(bin_edges[:1] + bin_edges[1:]) / 2,
+                 x=(bin_edges[:-1] + bin_edges[1:]) / 2,
                  y=hist,
                  style='k-',
                  ),
@@ -48,7 +48,7 @@ def height_distribution(topography, bins=None, wfac=5):
     )
 
 
-def slope_distribution(topography, bins=None):
+def slope_distribution(topography, bins=None, wfac=5):
     if bins is None:
         bins = int(np.sqrt(np.prod(topography.shape)) + 1.0)
 
@@ -61,6 +61,11 @@ def slope_distribution(topography, bins=None):
 
     hist, bin_edges = np.histogram(slope, bins=bins, density=True)
 
+    minval = mean_slope - wfac * rms_slope
+    maxval = mean_slope + wfac * rms_slope
+    x_gauss = np.linspace(minval, maxval, 1001)
+    y_gauss = np.exp(-(x_gauss - mean_slope) ** 2 / (2 * rms_slope ** 2)) / (np.sqrt(2 * np.pi) * rms_slope)
+
     return dict(
         name='Slope distribution',
         scalars=dict(
@@ -71,9 +76,14 @@ def slope_distribution(topography, bins=None):
         ylabel='Probability',
         series=[
             dict(name='Slope distribution',
-                 x=(bin_edges[:1] + bin_edges[1:]) / 2,
+                 x=(bin_edges[:-1] + bin_edges[1:]) / 2,
                  y=hist,
                  style='k-',
+                 ),
+            dict(name='Gaussian',
+                 x=x_gauss,
+                 y=y_gauss,
+                 style='r-',
                  )
         ]
     )