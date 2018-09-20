91f6f0d5e58967ac782d0a48fc49d6891acabac0
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2018-09-20 22:39:17 +0200

    ENH: Added admin interface for Surface model

--- topobank/manager/admin.py
+++ topobank/manager/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Topography
+from .models import Surface, Topography
+
+
+@admin.register(Surface)
+class SurfaceAdmin(admin.ModelAdmin):
+    pass
+
 
 @admin.register(Topography)
 class TopographyAdmin(admin.ModelAdmin):
     pass
-
