229299fee7d7a68be6d9315942298449eb36b190
Michael Röttger <roettger@tf.uni-freiburg.de>
2018-10-16 16:17:38 +0200

    TST: Added test for deleting topography. Closes #3.

--- browser_tests/test_topography_handling.py
+++ browser_tests/test_topography_handling.py
@@ -1,8 +1,73 @@
 
 import pytest
 import os, os.path
-
 from django.urls import reverse
+from selenium.common.exceptions import NoSuchElementException
+
+
+@pytest.mark.django_db
+def test_deleting_topography(one_empty_surface_testuser_signed_in, webdriver):
+
+    link = webdriver.find_element_by_link_text("Surfaces")
+    link.click()
+
+    datafile_path = 'topobank/manager/fixtures/example3.di'  # choose an existing data file which should work
+    datafile_path = os.path.join(os.getcwd(), datafile_path)
+
+    link = webdriver.find_element_by_link_text("Add Topography")
+
+    link.click()
+
+    input = webdriver.find_element_by_id("id_0-datafile")
+    input.send_keys(datafile_path)
+
+
+    # go to step 2
+    link = webdriver.find_element_by_id("submit-id-save")
+    link.click()
+
+    input = webdriver.find_element_by_id('id_1-name')
+    input.send_keys("Test Topography")
+
+    input = webdriver.find_element_by_id('id_1-measurement_date')
+    input.send_keys("2018-02-01") # should not be validated on cancel
+
+    # go to step 3
+    link = webdriver.find_element_by_id("submit-id-save")
+    link.click()
+
+    # finally save
+    link = webdriver.find_element_by_id("submit-id-save")
+    link.click()
+
+    #
+    # Now open topography
+    #
+    link = webdriver.find_element_by_link_text("Surfaces")
+    link.click()
+
+    link = webdriver.find_element_by_link_text("Surface 1")
+    link.click()
+
+    #
+    # Press delete
+    #
+    table_row = webdriver.find_element_by_class_name("clickable-table-row")
+    table_row.click()
+
+    link = webdriver.find_element_by_link_text("Delete")
+    link.click()
+
+    #
+    # Topography should not be shown any more
+    #
+    with pytest.raises(NoSuchElementException):
+        webdriver.find_element_by_class_name("clickable-table-row")
+
+
+
+
+
 
 @pytest.mark.django_db
 def test_cancel_while_creating_topography(one_empty_surface_testuser_signed_in, webdriver):
@@ -62,12 +127,7 @@ def test_cancel_while_creating_topography(one_empty_surface_testuser_signed_in, 
     link.click()
 
     input = webdriver.find_element_by_id("id_0-datafile")
-
-    datafile_path = os.path.join(os.getcwd(),
-                                 'topobank/manager/fixtures/example3.di')  # choose an existing data file which should work
-
     input.send_keys(datafile_path)
-
 
     # go to step 2
     link = webdriver.find_element_by_id("submit-id-save")
