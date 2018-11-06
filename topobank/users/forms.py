cf645bec321352069fa163585fb0d2d40695aafc
Michael Röttger <roettger@tf.uni-freiburg.de>
2018-11-06 12:35:32 +0100

    ENH: Closes #36, closes #37, closes #25.
    Using now email as user identifier, show full name in user dropdown, enforce user name entry on sign up.

---
+++ topobank/users/forms.py
@@ -0,0 +1,9 @@
+from allauth.account.forms import SignupForm
+from django import forms
+
+class SignupFormWithName(SignupForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'] = forms.CharField()
+
