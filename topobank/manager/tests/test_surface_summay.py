0e4b192d9ea63d3a8967bea517fed87ebc618772
Michael Röttger <roettger@tf.uni-freiburg.de>
2018-11-12 11:53:05 +0100

    TST: Extracted generation of mocks in fixture.

--- topobank/manager/tests/test_surface_summay.py
+++ topobank/manager/tests/test_surface_summay.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest import approx
 
 from dataclasses import dataclass
@@ -5,9 +6,11 @@ from dataclasses import dataclass
 from ..models import Topography
 from ..utils import bandwidths_data
 
-def test_bandwiths_data(mocker):
 
-    @dataclass # new feature in Python 3.7
+@pytest.fixture
+def two_topos_mock(mocker):
+
+    @dataclass  # new feature in Python 3.7
     class PyCoTopoStub:
         pixel_size: float
         size: float
@@ -29,22 +32,22 @@ def test_bandwiths_data(mocker):
     #   needed from PyCo's Topography: pixel_size, size
     #   needed from TopoBank's Topography: name, pk
 
-    topos = [ Topography(name='topoA', pk=1), Topography(name='topoB', pk=2)]
+    topos = [Topography(name='topoA', pk=1), Topography(name='topoB', pk=2)]
 
-    bd = bandwidths_data(topos)
+    return topos
+
+def test_bandwiths_data(two_topos_mock):
+
+    bd = bandwidths_data(two_topos_mock)
 
     exp_bd = [
         dict(upper_bound=1e-4, lower_bound=5e-6, name='topoA', link='linkA/'),
         dict(upper_bound=6e-7, lower_bound=6e-9, name='topoB', link='linkB/'),
     ]
 
-
     for i in [0,1]:
         for field in ['upper_bound', 'lower_bound']:
             assert exp_bd[i][field] == approx(bd[i][field])
         for field in ['name', 'link']:
             assert exp_bd[i][field] == bd[i][field]
-
-
-
 
