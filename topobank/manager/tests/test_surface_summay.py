3c3b609f73f172de3a18eb61dd7ae83181ef588f
Michael Röttger <roettger@tf.uni-freiburg.de>
2018-11-12 10:41:07 +0100

    TST: Added test for bandwidth data.

---
+++ topobank/manager/tests/test_surface_summay.py
@@ -0,0 +1,55 @@
+from pytest import approx
+
+from dataclasses import dataclass
+
+from ..models import Topography
+from ..utils import bandwidths_data
+
+def test_bandwiths_data(mocker):
+
+    @dataclass # new feature in Python 3.7
+    class PyCoTopoStub:
+        pixel_size: float
+        size: float
+        unit: str
+
+    @dataclass  # new feature in Python 3.7
+    class TopoStub:
+        name: str
+        pk: int
+
+    topography_method_mock = mocker.patch('topobank.manager.models.Topography.topography')
+    topography_method_mock.side_effect = [
+        PyCoTopoStub(pixel_size=5., size=100., unit='µm'),
+        PyCoTopoStub(pixel_size=6., size=600., unit='nm'),
+    ]
+    mocker.patch('topobank.manager.models.Topography', autospec=True)
+
+    reverse_patch = mocker.patch('topobank.manager.utils.reverse')
+    reverse_patch.side_effect = [
+        'linkA/', 'linkB/'
+    ]
+
+    # for bandwidths_data():
+    #   needed from PyCo's Topography: pixel_size, size
+    #   needed from TopoBank's Topography: name, pk
+
+    topos = [ Topography(name='topoA', pk=1), Topography(name='topoB', pk=2)]
+
+    bd = bandwidths_data(topos)
+
+    exp_bd = [
+        dict(upper_bound=1e-4, lower_bound=5e-6, name='topoA', link='linkA/'),
+        dict(upper_bound=6e-7, lower_bound=6e-9, name='topoB', link='linkB/'),
+    ]
+
+
+    for i in [0,1]:
+        for field in ['upper_bound', 'lower_bound']:
+            assert exp_bd[i][field] == approx(bd[i][field])
+        for field in ['name', 'link']:
+            assert exp_bd[i][field] == bd[i][field]
+
+
+
+
