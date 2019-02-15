BUG: Closes #121, bandwidth calculation failure for 2D topographies.

topobank/analysis/functions.py
@@ -520,6 +520,12 @@ def variable_bandwidth(topography):
     if not topography.is_uniform:
         raise NotImplementedError("Variable bandwidth hasn't been implemented for non-uniform topographies yet.")
 
+    if topography.dim == 1:
+        raise NotImplementedError("Variable bandwidth hasn't been implemented for uniform line scans yet.")
+
+    #
+    # Implementation for uniform 2D topographies
+    #
     size = topography.size
     scale_factor = 1
     no_exception = True
@@ -528,9 +534,10 @@ def variable_bandwidth(topography):
     while no_exception and scale_factor < 128:
         no_exception = False
         try:
-            s = checkerboard_tilt_correction(topography, sd=(scale_factor, )*topography.dim)
+            s = checkerboard_tilt_correction(topography, sd=(scale_factor, )*topography.dim) # returns an array
             bandwidths += [np.mean(size)/scale_factor]
-            rms_heights += [rms_height_uniform(s, kind='Sq' if s.dim == 2 else 'Rq')]
+            tmp_topography = Topography(s, topography.size, periodic=topography.is_periodic, info=topography.info)
+            rms_heights += [rms_height_uniform(tmp_topography, kind='Sq' if len(s.shape) == 2 else 'Rq')]
             no_exception = True
         except np.linalg.LinAlgError:
             pass

topobank/analysis/tests/test_functions.py
@@ -6,7 +6,7 @@ from PyCo.Topography import Topography, NonuniformLineScan
 
 from topobank.analysis.functions import (
     height_distribution, slope_distribution, curvature_distribution,
-    autocorrelation)
+    autocorrelation, variable_bandwidth)
 
 ###############################################################################
 # Tests for line scans
@@ -189,5 +189,22 @@ def test_autocorrelation_simple_2D_topography():
 
     assert result['name'] == 'Height-difference autocorrelation function (ACF)'
 
-    # TODO Check other values, too
+    # TODO Check result values for autocorrelation
 
+def test_variable_bandwidth_simple_2D_topography():
+    y = np.arange(10).reshape((1, -1))
+    x = np.arange(5).reshape((-1, 1))
+
+    arr = -2 * y + 0 * x  # only slope in y direction
+
+    info = dict(unit='nm')
+    t = Topography(arr, (10, 5), info=info).detrend('center')
+
+    # resulting heights follow this function: h(x,y)=-4y+9
+
+    result = variable_bandwidth(t)
+
+    assert sorted(result.keys()) == sorted(['name', 'xlabel', 'ylabel', 'xscale', 'yscale', 'xunit', 'yunit', 'series'])
+
+    assert result['name'] == 'Variable-bandwidth analysis'
+    # TODO Check result values for bandwidht