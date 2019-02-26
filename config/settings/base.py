0e826a467ba60ca90c7a6194bb7e44eb3ec29f77
Michael Röttger <roettger@tf.uni-freiburg.de>
2019-02-26 15:01:27 +0100

    WIP: Important celery config, forgotten in previous commits.

--- config/settings/base.py
+++ config/settings/base.py
@@ -233,21 +233,43 @@ MANAGERS = ADMINS
 # Celery
 # ------------------------------------------------------------------------------
 INSTALLED_APPS += ['topobank.taskapp.celery.CeleryAppConfig']
+if USE_TZ:
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-timezone
+    CELERY_TIMEZONE = TIME_ZONE
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-broker_url
+CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='django://')
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_backend
+CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND', default='rpc://') # CELERY_BROKER_URL
+CELERY_RESULT_PERSISTENT = True
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-accept_content
+CELERY_ACCEPT_CONTENT = ['json']
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_serializer
+CELERY_TASK_SERIALIZER = 'json'
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_serializer
+CELERY_RESULT_SERIALIZER = 'json' # TODO pickle because of arrays?
+# TODO: set to whatever value is adequate in your circumstances
+CELERYD_TASK_TIME_LIMIT = 5 * 60
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-soft-time-limit
+# TODO: set to whatever value is adequate in your circumstances
+CELERYD_TASK_SOFT_TIME_LIMIT = 60
+
+
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-broker_url
 #CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='django://')
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+#CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_backend
-if CELERY_BROKER_URL == 'django://':
-    CELERY_RESULT_BACKEND = 'redis://'
-else:
-    CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+#if CELERY_BROKER_URL == 'django://':
+#    CELERY_RESULT_BACKEND = 'redis://'
+#else:
+#    CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-accept_content
-CELERY_ACCEPT_CONTENT = ['json', 'pickle'] # TODO remove JSON?
+#CELERY_ACCEPT_CONTENT = ['json', 'pickle'] # TODO remove JSON?
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_serializer
-CELERY_TASK_SERIALIZER = 'pickle'
+#CELERY_TASK_SERIALIZER = 'pickle'
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_serializer
-CELERY_RESULT_SERIALIZER = 'pickle' # because of arrays
+#CELERY_RESULT_SERIALIZER = 'pickle' # because of arrays
+
 # django-allauth
 # ------------------------------------------------------------------------------
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
