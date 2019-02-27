c4af834322b0e5e85d19078620235a046c3be523
Michael Röttger <roettger@tf.uni-freiburg.de>
2019-02-27 17:48:56 +0100

    WIP: Changed media and caddy settings.

--- config/settings/base.py
+++ config/settings/base.py
@@ -165,7 +165,7 @@ STATICFILES_FINDERS = [
 # MEDIA
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
-MEDIA_ROOT = str(APPS_DIR('media'))
+MEDIA_ROOT = str(ROOT_DIR.path('media'))
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
 MEDIA_URL = '/media/'
 
--- config/settings/production.py
+++ config/settings/production.py
@@ -112,7 +112,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     'DJANGO_DEFAULT_FROM_EMAIL',
-    default='TopoBank <noreply@example.com>'
+    default='TopoBank <noreply@contact.engineering>'
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
