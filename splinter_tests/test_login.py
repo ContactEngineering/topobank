fbdc1df077a29d64b8fb880acd1b2dbdfa75760b
Michael Röttger <roettger@tf.uni-freiburg.de>
2019-05-29 10:49:15 +0200

    TST: Added first splinter test for browser testing. Related to #11.
    This also prepares tests for #83 and is needed to run javascript
    related tests and other integration tests from a user perspective.

--- splinter_tests/test_login.py
+++ splinter_tests/test_login.py
@@ -12,15 +12,27 @@ def user_alice(django_user_model):
 @pytest.fixture
 def user_alice_logged_in(live_server, browser, user_alice):
     browser.visit(live_server.url + "/accounts/login")  # we don't want to use ORCID here
+
+    #
+    # Logging in
+    #
     browser.fill('login', user_alice.username)
     browser.fill('password', PASSWORD)
-    btn = browser.find_by_text("Sign In")
+    btn = browser.find_by_text('Sign In').last
     btn.click()
+
+    yield
+
+    #
+    # Logging out
+    #
+    browser.find_by_id("userDropdown").click()
+    browser.find_by_text("Sign Out").first.click()
+    browser.is_element_present_by_text("Ready to Leave?", wait_time=1)
+    browser.find_by_text("Sign Out").last.click()
 
 
 def test_login(browser, user_alice_logged_in):
+    assert browser.is_text_present("Welcome to contact.engineering")
 
-    dropdown = browser.find_by_text('Alice Wonderland')
-    dropdown.click()
-    logout_link = browser.find_by_text('Sign Out')
-    logout_link.click()
+
