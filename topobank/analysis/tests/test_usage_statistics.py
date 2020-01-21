31cc2f147cc5efefc4930bdd1486229c09340fb8
Michael Röttger <roettger@tf.uni-freiburg.de>
2020-01-21 12:05:59 +0100

    WIP: Added test for couting analysis result views. Related to #147.

--- topobank/analysis/tests/test_usage_statistics.py
+++ topobank/analysis/tests/test_usage_statistics.py
@@ -1,16 +1,17 @@
 import pytest
 from django.shortcuts import reverse
-from django.contrib.contenttypes.models import ContentType
-from trackstats.models import Metric, StatisticByDateAndObject
+from trackstats.models import Metric, Period
 
 from topobank.analysis.tests.utils import AnalysisFactory
 
 @pytest.mark.django_db
-def test_counts_analyses_views(client, handle_usage_statistics):
+def test_counts_analyses_views(client, mocker, handle_usage_statistics):
     analysis = AnalysisFactory()
     topography = analysis.topography
     function = analysis.function
     user = analysis.topography.surface.creator
+
+    metric = Metric.objects.ANALYSES_RESULTS_VIEW_COUNT
 
     client.force_login(user)
 
@@ -26,22 +27,16 @@ def test_counts_analyses_views(client, handle_usage_statistics):
         }, HTTP_X_REQUESTED_WITH='XMLHttpRequest')  # we need an AJAX request
         return response
 
+    record_mock = mocker.patch('trackstats.models.StatisticByDateAndObject.objects.record')
+
     response = send_card_request()  # first send request, then request metrics! Otherwise it does not exist.
     assert response.status_code == 200
 
-    metric = Metric.objects.get(ref='analyses_results_view_count')
-    ct = ContentType.objects.get(app_label='analysis', model='analysisfunction')
-
-    def get_function_view_entries():
-        return StatisticByDateAndObject.objects.filter(metric=metric,
-                                                       object_id=function.id,
-                                                       object_type_id=ct.id)
-
-
-    assert get_function_view_entries().count() == 1
-
+    record_mock.assert_called_with(metric=metric, object=function, period=Period.DAY, value=1)
     response = send_card_request()
     assert response.status_code == 200
-    assert get_function_view_entries().count() == 2
+
+    assert record_mock.call_count == 2
 
     client.logout()
+
--- topobank/manager/tests/test_utils.py
+++ topobank/manager/tests/test_utils.py
@@ -7,10 +7,10 @@ import pytest
 from pathlib import Path
 import io
 
-from ..tests.utils import two_topos
+from ..tests.utils import two_topos, TopographyFactory
 from ..utils import DEFAULT_DATASOURCE_NAME, \
     selection_to_instances, selection_for_select_all, selection_choices, instances_to_selection, \
-    tags_for_user
+    tags_for_user, increase_statistics_by_date_and_object, increase_statistics_by_date
 from ..models import Surface, Topography
 
 # def test_data_sources_txt():
@@ -147,4 +147,64 @@ def test_tags_for_user(two_topos):
 
     assert set( t.name for t in tags) == {'a long tag with spaces', 'interesting', 'rare', 'rough',
                                           'projects/a', 'projects/b', 'projects/c', 'projects'}
+
+
+@pytest.mark.django_db
+def test_increase_statistics_by_date(handle_usage_statistics):
+
+    from trackstats.models import Metric, Domain, StatisticByDate
+
+    Domain.objects.TESTDOMAIN = Domain.objects.register(ref='test', name='A test domain')
+    Metric.objects.TESTMETRIC = Metric.objects.register(ref='test', name='A test metric',
+                                                        domain=Domain.objects.TESTDOMAIN)
+
+    metric = Metric.objects.TESTMETRIC
+
+
+    increase_statistics_by_date(metric, increment=2)
+
+    s = StatisticByDate.objects.get(metric=metric)
+    assert s.value == 2
+
+    increase_statistics_by_date(metric)
+
+    s = StatisticByDate.objects.get(metric=metric)
+    assert s.value == 3
+
+
+@pytest.mark.django_db
+def test_increase_statistics_by_date_and_object(handle_usage_statistics):
+
+    from trackstats.models import Metric, Domain, StatisticByDateAndObject
+
+    Domain.objects.TESTDOMAIN = Domain.objects.register(ref='test', name='A test domain')
+    Metric.objects.TESTMETRIC = Metric.objects.register(ref='test', name='A test metric',
+                                                        domain=Domain.objects.TESTDOMAIN)
+
+    metric = Metric.objects.TESTMETRIC
+
+    topo1 = TopographyFactory()
+    topo2 = TopographyFactory()
+
+    #
+    # Increase counter for topo1
+    #
+    increase_statistics_by_date_and_object(metric, obj=topo1, increment=2)
+
+    s = StatisticByDateAndObject.objects.get(metric=metric, object_id=topo1.id)
+    assert s.value == 2
+
+    increase_statistics_by_date_and_object(metric, obj=topo1)
+
+    s = StatisticByDateAndObject.objects.get(metric=metric, object_id=topo1.id)
+    assert s.value == 3
+
+    #
+    # Increase counter for topo2
+    #
+    increase_statistics_by_date_and_object(metric, obj=topo2)
+    s = StatisticByDateAndObject.objects.get(metric=metric, object_id=topo1.id)
+    assert s.value == 3
+    s = StatisticByDateAndObject.objects.get(metric=metric, object_id=topo2.id)
+    assert s.value == 1
 
