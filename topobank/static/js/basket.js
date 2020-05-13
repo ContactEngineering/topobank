7a2fd61aec14b7a5ae45534717ba360675cd3d1e
Michael Röttger <roettger@tf.uni-freiburg.de>
2020-05-13 18:16:14 +0200

    ENH: Added medium font size to basket elements in order to make them more visible.

--- topobank/static/js/basket.js
+++ topobank/static/js/basket.js
@@ -83,17 +83,17 @@ Vue.component('basket-element', {
     props: ['elem'], // object with keys: 'name', 'type'
     delimiters: ['[[', ']]'],
     template: `
-           <span v-if="elem.type=='surface'" class="badge badge-pill badge-primary mr-1">
+           <span v-if="elem.type=='surface'" class="badge badge-pill badge-primary mr-1 basket-element">
                 <span class="fa fa-diamond"></span>
                 [[ elem.name ]]
                 <span class="fa fa-close" v-on:click="handle_close"></span>
            </span>
-           <span v-else-if="elem.type=='tag'" class="badge badge-pill badge-info mr-1">
+           <span v-else-if="elem.type=='tag'" class="badge badge-pill badge-info mr-1 basket-element">
                 <span class="fa fa-tag"></span>
                 [[ elem.name ]]
                 <span class="fa fa-close" v-on:click="handle_close"></span>
            </span>
-           <span v-else class="badge badge-pill badge-secondary mr-1">
+           <span v-else class="badge badge-pill badge-secondary mr-1 basket-element">
                 <span class="fa fa-file"></span>
                 [[ elem.name ]]
                 <span class="fa fa-close" v-on:click="handle_close"></span>
