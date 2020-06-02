b17fee422df340fad6cac7584c9a36575edc7b7c
Michael Röttger <roettger@tf.uni-freiburg.de>
2020-06-02 14:08:45 +0200

    TST: Fixing second simple splinter test.

--- splinter_tests/test_surface_creation.py
+++ splinter_tests/test_surface_creation.py
@@ -3,9 +3,9 @@ def test_empty_surface(browser, user_alice_logged_in):
     #
     # navigate to surface list and create surface
     #
-    surfaces_link = browser.find_link_by_partial_text('Surfaces')
+    surfaces_link = browser.find_link_by_partial_text('Select')
     surfaces_link.click()
-    create_link = browser.find_link_by_partial_text('Add Surface')
+    create_link = browser.find_link_by_partial_text('Create Surface')
     create_link.click()
 
     browser.fill('name', 'My first empty surface')
@@ -19,22 +19,13 @@ def test_empty_surface(browser, user_alice_logged_in):
     #
     # Navigate back to surface list
     #
-    surfaces_link = browser.find_link_by_partial_text('Surfaces')
+    surfaces_link = browser.find_link_by_partial_text('Select')
     surfaces_link.click()
 
     #
     # Select new surface
     #
-    browser.find_option_by_text('My first empty surface').first.click()
-
-    save_link = browser.find_by_id('submit-id-save')
-    save_link.click()
-
-    #
-    # Now the link "Open" should be visible
-    # and we should be on the detail page for the surface
-    #
-    browser.find_link_by_partial_text('Open').first.click()
+    browser.find_link_by_partial_text('Properties').first.click()
 
     assert "My first empty surface" in browser.html
 
