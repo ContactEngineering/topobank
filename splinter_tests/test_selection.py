a70d626eb1ef263904115363e4e8ba10f6d3ddd3
Michael Röttger <roettger@tf.uni-freiburg.de>
2020-06-16 15:18:34 +0200

    TST: Added splinter test for selecting page size. Closes #469.

--- splinter_tests/test_selection.py
+++ splinter_tests/test_selection.py
@@ -70,3 +70,79 @@ def test_deselect_all(browser, user_alice_logged_in, items_for_selection):
     browser.quit()
 
 
+@pytest.mark.django_db
+def test_select_page_size(browser, user_alice_logged_in):
+
+    # create a lot of surfaces
+    for i in range(11):
+        SurfaceFactory(creator=user_alice_logged_in)
+
+    #
+    # navigate to select page
+    #
+    select_link = browser.find_link_by_partial_text('Select')
+    select_link.click()
+
+    #
+    # pagination should have 2 pages
+    #
+    pagination = browser.find_by_id("pagination")
+
+    # there should be 4 items: previous, 1, 2, next
+    assert browser.is_text_present("Next", wait_time=1)
+
+    page_items = pagination.find_by_css(".page-item")
+
+    assert len(page_items) == 4
+
+    assert page_items[0].text == "Previous"
+    assert page_items[1].text == "1"
+    assert page_items[2].text == "2"
+    assert page_items[3].text == "Next"
+
+    # page size should show "10"
+    page_size_select = browser.find_by_id("page-size-select")
+    assert page_size_select.find_by_css(".selected").first.text == "10"
+
+    # footer should show total number
+    assert browser.is_text_present("Showing 10 surfaces out of 11")
+
+    # press "Next"
+    page_items[3].click()
+
+    # now footer shows different text
+    assert browser.is_text_present("Showing 1 surfaces out of 11", wait_time=1)
+
+    # select page size 25
+    page_size_25_option = page_size_select.find_by_css('option')[1]
+    assert page_size_25_option.text == "25"
+    page_size_25_option.click()
+
+    # now there is only one page
+    assert browser.is_text_present("Next", wait_time=1)
+    assert browser.is_text_present("Showing 11 surfaces out of 11", wait_time=1)
+
+    pagination = browser.find_by_id("pagination")
+    page_items = pagination.find_by_css(".page-item")
+
+    assert len(page_items) == 3
+
+    assert page_items[0].text == "Previous"
+    assert page_items[1].text == "1"
+    assert page_items[2].text == "Next"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
