9492f238196652aaac1c595ff5d721fea0a40f8b
Michael Röttger <roettger@tf.uni-freiburg.de>
2020-07-08 17:33:56 +0200

    WIP: Added test for management command. Related to #317.

--- topobank/users/tests/test_groups.py
+++ topobank/users/tests/test_groups.py
@@ -1,8 +1,34 @@
 import pytest
 
+from django.core.management import call_command
+
+from .test_utils import UserFactory
 from ..utils import get_default_group
 from topobank.users.models import DEFAULT_GROUP_NAME
 
+
+@pytest.mark.django_db
+def test_management_command_for_default_group():
+
+    user = UserFactory()
+    from django.contrib.auth.models import Group
+
+    # remove group if existing
+    try:
+        group = Group.objects.get(name=DEFAULT_GROUP_NAME)
+        user.groups.remove(group)
+        group.delete()
+    except Group.DoesNotExist:
+        pass
+
+    assert user.groups.count() == 0
+
+    call_command('ensure_default_group')
+    from django.contrib.auth.models import Group
+    default_group = Group.objects.get(name=DEFAULT_GROUP_NAME)
+
+    assert user.groups.count() == 1
+    assert user.groups.first() == default_group
 
 @pytest.mark.django_db
 def test_default_group_helper():
