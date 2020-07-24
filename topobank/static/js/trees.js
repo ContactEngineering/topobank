46df6463674e2f1ce3a62d81b6f10261343d22ac
Michael Röttger <roettger@tf.uni-freiburg.de>
2020-07-24 14:56:33 +0200

    WIP: Added "published" badge to published elements in list.
    Related to #317.
    Additionally the badges for "category" are now class 'badge-secondary'.

--- topobank/static/js/trees.js
+++ topobank/static/js/trees.js
@@ -183,6 +183,12 @@ let search_results_vm = new Vue({
                           $tdList
                               .eq(1)
                               .html(version_html);
+
+                          // Also add badge for "published" to first column
+                          if (node.data.version.toString().length > 0) {
+                              published_html = "<span class='badge badge-info mr-1'>published</span>";
+                              $tdList.eq(0).find('.fancytree-title').after(published_html);
+                          }
                       }
 
                       // Set column with description
