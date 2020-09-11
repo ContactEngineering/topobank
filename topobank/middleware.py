55e26832e08412df79155927df06b9f661f4bc6a
Michael Röttger <roettger@tf.uni-freiburg.de>
2020-09-11 13:44:03 +0200

    ENH: Finished implementation of anonymous sessions. Closes #164.
    The account login URL is also excluded here in the middleware because
    we need this for the splinter tests.

--- topobank/middleware.py
+++ topobank/middleware.py
@@ -3,6 +3,7 @@ from guardian.shortcuts import get_anonymous_user
 
 # some abbreviations in order to save time on every request
 ACCOUNT_SIGNUP_URL = reverse('account_signup')
+ACCOUNT_LOGIN_URL = reverse('account_login')
 
 
 def anonymous_user_middleware(get_response):
@@ -23,12 +24,14 @@ def anonymous_user_middleware(get_response):
     def middleware(request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        if not (request.user.is_authenticated or request.path == ACCOUNT_SIGNUP_URL):
+        if not (request.user.is_authenticated
+                or request.path == ACCOUNT_SIGNUP_URL or request.path == ACCOUNT_LOGIN_URL):
             request.user = get_anonymous_user()
 
         # mostly, we replace the anonymous user with guardian's AnonymousUser,
         # except when the URL for account signup is called. This is needed
-        # for a test
+        # for a test. Same for account login, this is needed for the browser
+        # tests, because we don't wont to login with an orcid there.
 
         response = get_response(request)
 
