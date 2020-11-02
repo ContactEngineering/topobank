3692dd35252d55216cc9bb61df81c61410fa422c
Michael Röttger <roettger@tf.uni-freiburg.de>
2020-11-02 08:38:13 +0100

    TST: Test which shows problem. Related to #528.

--- topobank/manager/tests/test_welcome_page.py
+++ topobank/manager/tests/test_welcome_page.py
@@ -1,28 +1,44 @@
-
+import pytest
 from django.shortcuts import reverse
 
-from topobank.manager.tests.utils import SurfaceFactory, TopographyFactory
+from topobank.manager.tests.utils import SurfaceFactory, TopographyFactory, UserFactory
 from topobank.analysis.tests.utils import AnalysisFactory
 
 from topobank.utils import assert_in_content
 
-def test_welcome_page_statistics(client, django_user_model):
 
-    password = "secret"
+@pytest.mark.django_db
+@pytest.fixture
+def test_instances():
 
-    #
-    # create some database objects
-    #
-    user_1 = django_user_model.objects.create_user(username='user1', password=password)
-    user_2 = django_user_model.objects.create_user(username='user2', password=password)
+    users = [
+        UserFactory(username='user1'),
+        UserFactory(username='user2')
+    ]
 
-    surface_1 = SurfaceFactory(creator=user_1)
-    surface_2 = SurfaceFactory(creator=user_1)
-    topography_1 = TopographyFactory(surface=surface_1)
+    surfaces = [
+        SurfaceFactory(creator=users[0]),
+        SurfaceFactory(creator=users[0]),
+    ]
 
+    topographies = [
+        TopographyFactory(surface=surfaces[0])
+    ]
+
+    AnalysisFactory(topography=topographies[0])
+
+    return users, surfaces, topographies
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('with_publication', [False, True])
+def test_welcome_page_statistics(client, test_instances, with_publication):
+
+    (user_1, user_2), (surface_1, surface_2), (topography_1,) = test_instances
     surface_2.share(user_2)
 
-    AnalysisFactory(topography=topography_1)
+    if with_publication:
+        surface_1.publish('cc0-1.0', 'Issac Newton')
 
     #
     # Test statistics if user is not yet authenticated
@@ -37,7 +53,7 @@ def test_welcome_page_statistics(client, django_user_model):
     #
     # Test statistics if user_1 is authenticated
     #
-    assert client.login(username=user_1.username, password=password)
+    client.force_login(user_1)
     response = client.get(reverse('home'))
 
     assert_in_content(response, '<div class="welcome-page-statistics">2</div> surfaces in database')
@@ -50,7 +66,7 @@ def test_welcome_page_statistics(client, django_user_model):
     #
     # Test statistics if user_2 is authenticated
     #
-    assert client.login(username=user_2.username, password=password)
+    client.force_login(user_2)
     response = client.get(reverse('home'))
 
     assert_in_content(response, '<div class="welcome-page-statistics">0</div> surfaces in database')
@@ -59,3 +75,5 @@ def test_welcome_page_statistics(client, django_user_model):
     assert_in_content(response, '<div class="welcome-page-statistics">1</div> surfaces with you')
 
     client.logout()
+
+
