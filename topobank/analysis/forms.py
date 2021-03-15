7fe07fdaa0cfddcfab9a62694c4dd14160aa03be
Michael Röttger <roettger@tf.uni-freiburg.de>
2021-03-15 13:10:04 +0100

    ENH: Added "(Average)" to the function labels where appropriate.
    Also added description to the help section.
    
    Related to #365.

--- topobank/analysis/forms.py
+++ topobank/analysis/forms.py
@@ -1,20 +1,42 @@
 from django.forms import ModelMultipleChoiceField, forms, CheckboxSelectMultiple
+from django.contrib.contenttypes.models import ContentType
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, HTML, Div
 from crispy_forms.bootstrap import (FormActions, InlineCheckboxes)
 
 from .models import AnalysisFunction
+from ..manager.models import Surface
+
+
+class FunctionChoiceField(ModelMultipleChoiceField):
+    """Field for choosing an Analysis function.
+
+    Subclassed here for added a custom representation including the implemented types.
+    """
+    def label_from_instance(self, function):
+        """Return a custom representation based on an instance."""
+        label = str(function)
+
+        if function.is_implemented_for_type(ContentType.objects.get_for_model(Surface)):
+            label = "(Average) "+label
+
+        return label
+
 
 class FunctionSelectForm(forms.Form):
+    """Form for selecting an analysis function."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['functions'].label = False
 
-    functions = ModelMultipleChoiceField(queryset=AnalysisFunction.objects.all(),
-                                         widget=CheckboxSelectMultiple,
-                                         help_text="Select one or multiple analysis functions.")
+    help_text = "Select one or multiple analysis functions. Functions annotated with '(Average)' also " \
+                "show an average for all measurements for a surface if there is more than one."
+
+    functions = FunctionChoiceField(queryset=AnalysisFunction.objects.all(),
+                                    widget=CheckboxSelectMultiple,
+                                    help_text=help_text)
 
     helper = FormHelper()
     helper.form_method = 'POST'
