dbd495de44d8b55437d324709887c96add7252e9
Michael Röttger <roettger@tf.uni-freiburg.de>
2021-03-15 11:46:57 +0100

    TST: Added more tests for analyses renewal.

--- topobank/manager/tests/test_analyses_renewal.py
+++ topobank/manager/tests/test_analyses_renewal.py
@@ -2,9 +2,12 @@
 Test whether analyses are recalculated on certain events.
 """
 import pytest
+
+from pathlib import Path
 from django.shortcuts import reverse
 
-from .utils import TopographyFactory, SurfaceFactory, UserFactory
+from .utils import FIXTURE_DIR, TopographyFactory, SurfaceFactory, UserFactory
+from topobank.utils import assert_in_content, assert_no_form_errors
 
 
 @pytest.mark.django_db
@@ -43,7 +46,7 @@ def test_surface_analysis_renewal_on_topography_change(client, mocker):
 
 
 @pytest.mark.django_db
-def test_surface_analysis_renewal_on_topography_deletion(client, mocker):
+def test_surface_analysis_renewal_on_topography_deletion(client, mocker, handle_usage_statistics):
     """Check whether methods for renewal are called if topography is deleted.
     """
 
@@ -60,4 +63,74 @@ def test_surface_analysis_renewal_on_topography_deletion(client, mocker):
     assert response.status_code == 302
     assert renew_surf_analyses_mock.called
 
+
+@pytest.mark.django_db
+def test_surface_analysis_renewal_on_topography_creation(client, mocker, handle_usage_statistics):
+
+    renew_surf_analyses_mock = mocker.patch('topobank.manager.models.Surface.renew_analyses')
+    renew_topo_analyses_mock = mocker.patch('topobank.manager.models.Topography.renew_analyses')
+
+    user = UserFactory()
+    surface = SurfaceFactory(creator=user)
+    client.force_login(user)
+
+    #
+    # open first step of wizard: file upload
+    #
+    input_file_path = Path(FIXTURE_DIR + '/example-2d.npy')  # maybe use package 'pytest-datafiles' here instead
+    with open(str(input_file_path), mode='rb') as fp:
+        response = client.post(reverse('manager:topography-create',
+                                       kwargs=dict(surface_id=surface.id)),
+                               data={
+                                   'topography_create_wizard-current_step': 'upload',
+                                   'upload-datafile': fp,
+                                   'upload-datafile_format': '',
+                                   'upload-surface': surface.id,
+                               }, follow=True)
+
+    assert response.status_code == 200
+    assert_no_form_errors(response)
+
+    #
+    # now we should be on the page with second step
+    #
+    assert_in_content(response, "Step 2 of 3")
+    assert_in_content(response, '<option value="0">Default</option>')
+    assert response.context['form'].initial['name'] == 'example-2d.npy'
+
+    #
+    # Send data for second page
+    #
+    response = client.post(reverse('manager:topography-create',
+                                   kwargs=dict(surface_id=surface.id)),
+                           data={
+                               'topography_create_wizard-current_step': 'metadata',
+                               'metadata-name': 'topo1',
+                               'metadata-measurement_date': '2020-10-21',
+                               'metadata-data_source': 0,
+                               'metadata-description': "description",
+                           }, follow=True)
+    assert_no_form_errors(response)
+
+    #
+    # Send data for third page
+    #
+    assert_in_content(response, "Step 3 of 3")
+    response = client.post(reverse('manager:topography-create',
+                                   kwargs=dict(surface_id=surface.id)),
+                           data={
+                               'topography_create_wizard-current_step': 'units',
+                               'units-size_x': '1',
+                               'units-size_y': '1',
+                               'units-unit': 'nm',
+                               'units-height_scale': 1,
+                               'units-detrend_mode': 'height',
+                               'units-resolution_x': 2,
+                               'units-resolution_y': 2,
+                           }, follow=True)
+
+    assert_no_form_errors(response)
+
+    assert renew_topo_analyses_mock.called
+    assert renew_surf_analyses_mock.called
 
