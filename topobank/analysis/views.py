4244391b10ea9b76e6dbafb415953639433d03bd
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-04-28 17:10:36 +0200

    MAINT: Update label font size

--- topobank/analysis/views.py
+++ topobank/analysis/views.py
@@ -1056,6 +1056,8 @@ def _configure_plot(plot):
     plot.yaxis.axis_label_text_font_style = "normal"
     plot.xaxis.major_label_text_font_size = "12pt"
     plot.yaxis.major_label_text_font_size = "12pt"
+    plot.xaxis.axis_label_text_font_size = "12pt"
+    plot.yaxis.axis_label_text_font_size = "12pt"
     plot.xaxis.formatter = FuncTickFormatter(code="return format_exponential(tick);")
     plot.yaxis.formatter = FuncTickFormatter(code="return format_exponential(tick);")
 
