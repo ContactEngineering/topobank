ac5c068937be524e16755cffd9273dd904d1d6fc
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-05-03 11:50:26 +0200

    MAINT: Adjust default Bokeh font size to Bootstrap's settings

--- topobank/plots.py
+++ topobank/plots.py
@@ -4,9 +4,9 @@ def configure_plot(plot):
     plot.toolbar.logo = None
     plot.xaxis.axis_label_text_font_style = "normal"
     plot.yaxis.axis_label_text_font_style = "normal"
-    plot.xaxis.major_label_text_font_size = "12pt"
-    plot.yaxis.major_label_text_font_size = "12pt"
-    plot.xaxis.axis_label_text_font_size = "12pt"
-    plot.yaxis.axis_label_text_font_size = "12pt"
+    plot.xaxis.major_label_text_font_size = "1rem"
+    plot.yaxis.major_label_text_font_size = "1rem"
+    plot.xaxis.axis_label_text_font_size = "1rem"
+    plot.yaxis.axis_label_text_font_size = "1rem"
     plot.xaxis.formatter = FuncTickFormatter(code="return format_exponential(tick);")
     plot.yaxis.formatter = FuncTickFormatter(code="return format_exponential(tick);")
