5df8650ef81f23bf9f7cee514ba0f4beca4483a4
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2022-02-10 10:45:09 +0100

    TST: Parameterize card tests for 'simple' and 'plot' flavors

--- topobank/analysis/tests/test_cards.py
+++ topobank/analysis/tests/test_cards.py
@@ -12,8 +12,12 @@ from .utils import AnalysisFunctionFactory, TopographyAnalysisFactory, SurfaceAn
 from ..views import card_view_class, SimpleCardView, PlotCardView
 
 
+@pytest.mark.parametrize('card_view_flavor,list_template,detail_template',
+                         [('simple', 'analysis/simple_card_list.html', 'analysis/simple_card_detail.html'),
+                          ('plot', 'analysis/plot_card_list.html', 'analysis/plot_card_detail.html')])
 @pytest.mark.django_db
-def test_card_templates_simple(client, mocker, handle_usage_statistics):
+def test_card_templates_simple(client, mocker, handle_usage_statistics, card_view_flavor, list_template,
+                               detail_template):
     """Check whether correct template is selected."""
 
     #
@@ -21,47 +25,7 @@ def test_card_templates_simple(client, mocker, handle_usage_statistics):
     #
     password = "secret"
     user = UserFactory(password=password)
-    func1 = AnalysisFunctionFactory(card_view_flavor='power spectrum')
-    topo1 = Topography1DFactory()
-
-    # An analysis function with card_view_flavor='power spectrum'
-    # should use the template which is needed for PowerSpectrumCardView.
-    #
-    # For the "detail" mode, there is an own template for power spectrum,
-    # which should be returned. The the "list" mode, there is no
-    # special template. Therefore, since "PowerSpectrumCardView" is
-    # derived from the "PlotCardView" so far, the resulting
-    # template should be 'plot_card_list.html'.
-
-    assert client.login(username=user.username, password=password)
-
-    response = client.post(reverse('analysis:card'), data={
-        'function_id': func1.id,
-        'card_id': 'card',
-        'template_flavor': 'list',
-        'subjects_ids_json': subjects_to_json([topo1]),
-    }, HTTP_X_REQUESTED_WITH='XMLHttpRequest')  # we need an AJAX request
-
-    assert response.template_name == ['analysis/plot_card_list.html']
-
-    response = client.post(reverse('analysis:card'), data={
-        'function_id': func1.id,
-        'card_id': 'card',
-        'template_flavor': 'detail',
-        'subjects_ids_json': subjects_to_json([topo1]),
-    }, HTTP_X_REQUESTED_WITH='XMLHttpRequest')  # we need an AJAX request
-
-    assert response.template_name == ['analysis/powerspectrum_card_detail.html']
-
-
-@pytest.mark.django_db
-def test_card_templates_for_power_spectrum(client, mocker, handle_usage_statistics):
-    #
-    # Create database objects
-    #
-    password = "secret"
-    user = UserFactory(password=password)
-    func1 = AnalysisFunctionFactory(card_view_flavor='power spectrum')
+    func1 = AnalysisFunctionFactory(card_view_flavor=card_view_flavor)
     topo1 = Topography1DFactory()
 
     assert client.login(username=user.username, password=password)
@@ -73,9 +37,7 @@ def test_card_templates_for_power_spectrum(client, mocker, handle_usage_statisti
         'subjects_ids_json': subjects_to_json([topo1]),
     }, HTTP_X_REQUESTED_WITH='XMLHttpRequest')  # we need an AJAX request
 
-    # we get the inherited "plot" template with "list" flavor, because power spectrum
-    # hasn't got an own template with "list" flavor
-    assert response.template_name == ['analysis/plot_card_list.html']
+    assert response.template_name == [list_template]
 
     response = client.post(reverse('analysis:card'), data={
         'function_id': func1.id,
@@ -84,8 +46,7 @@ def test_card_templates_for_power_spectrum(client, mocker, handle_usage_statisti
         'subjects_ids_json': subjects_to_json([topo1]),
     }, HTTP_X_REQUESTED_WITH='XMLHttpRequest')  # we need an AJAX request
 
-    # for the power spectrum detail card there should be an own template
-    assert response.template_name == ['analysis/powerspectrum_card_detail.html']
+    assert response.template_name == [detail_template]
 
 
 @pytest.mark.django_db
@@ -97,7 +58,7 @@ def test_plot_card_if_no_successful_topo_analysis(client, handle_usage_statistic
     user = UserFactory(password=password)
     topography_ct = ContentType.objects.get_for_model(Topography)
     surface_ct = ContentType.objects.get_for_model(Surface)
-    func1 = AnalysisFunctionFactory(card_view_flavor='power spectrum')
+    func1 = AnalysisFunctionFactory(card_view_flavor='plot')
     AnalysisFunctionImplementationFactory(function=func1, subject_type=topography_ct)
     AnalysisFunctionImplementationFactory(function=func1, subject_type=surface_ct)
 
