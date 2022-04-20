BUG: Plastic calculation should reset plastic displacement before each optimizer run, closes #827

topobank/analysis/functions.py
@@ -913,7 +913,18 @@ def _next_contact_step(system, history=None, pentol=None, maxiter=None):
         History of contact calculations.
     """
 
+    # Get topography object from contact system
     topography = system.surface
+
+    try:
+        # Reset plastic displacement if this is a plastic calculation. We need to do this because the individual steps
+        # are not in order, i.e. the contact is not continuously formed or lifted. Each calculation needs to compute
+        # a fresh plastic displacement.
+        topography.plastic_displ = np.zeros_like(topography.plastic_displ)
+    except AttributeError:
+        pass
+
+    # Get substrate object from contact system
     substrate = system.substrate
 
     # Get the profile as a numpy array
@@ -1005,16 +1016,25 @@ def _contact_at_given_load(system, external_force, history=None, pentol=None, ma
         History of contact calculations.
     """
 
+    # Get topography object from contact system
     topography = system.surface
+
+    try:
+        # Reset plastic displacement if this is a plastic calculation. We need to do this because the individual steps
+        # are not in order, i.e. the contact is not continuously formed or lifted. Each calculation needs to compute
+        # a fresh plastic displacement.
+        topography.plastic_displ = np.zeros_like(topography.plastic_displ)
+    except AttributeError:
+        pass
+
+    # Get substrate object from contact system
     substrate = system.substrate
 
     # Get the profile as a numpy array
     heights = topography.heights()
 
     # Find max, min and mean heights
-    top = np.max(heights)
     middle = np.mean(heights)
-    bot = np.min(heights)
 
     if history is None:
         mean_displacements = []