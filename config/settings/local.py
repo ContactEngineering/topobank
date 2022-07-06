dc2ef679f1e92eba34049dbc1a2603418854052e
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2022-07-06 17:11:23 +0200

    MAINT: Added cprofiler middleware

--- config/settings/local.py
+++ config/settings/local.py
@@ -63,7 +63,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.ManifestStaticFilesStorage'
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
 INSTALLED_APPS += ['debug_toolbar']  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa F405
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',
+               'django_cprofile_middleware.middleware.ProfilerMiddleware']  # noqa F405
 # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
 DEBUG_TOOLBAR_CONFIG = {
     'DISABLE_PANELS': [
