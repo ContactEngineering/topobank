736a4b0ea5025c74281d02465880137261e553ba
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2022-07-08 17:58:43 +0200

    TST: Added test for supplementary option of `store_split_dict`

--- topobank/test_utils.py
+++ topobank/test_utils.py
@@ -1,4 +1,7 @@
 """Tests for utils module on topobank level"""
+
+import json
+
 from django.core.files.storage import default_storage
 
 import numpy as np
@@ -37,7 +40,40 @@ def test_store_split_dict_with_nan():
     assert np.array_equal(result_from_storage['series'][0]['y'], series_0.dict['y'], equal_nan=True)
 
 
+def test_store_split_dict_with_supplementary():
+    storage_prefix = "test"
 
+    supplementary = {"suppl": "info"}
+    series_0 = SplitDictionaryHere("series-0", {
+        "x": [1, 2, 3],
+        "y": np.array([5, 2, 11]),
+    }, supplementary=supplementary)
 
+    result_dict = {
+        "name": "Test result",
+        "series": [series_0],
+    }
 
+    store_split_dict(storage_prefix, "result", result_dict)
 
+    #
+    # Check whether load_split_dict can reproduce the result
+    #
+    result_from_storage = load_split_dict(storage_prefix, "result")
+    assert result_from_storage['name'] == result_dict['name']
+    assert np.array_equal(result_from_storage['series'][0]['x'], series_0.dict['x'])
+    assert np.array_equal(result_from_storage['series'][0]['y'], series_0.dict['y'], equal_nan=True)
+
+    #
+    # The toplevel JSON should contain the supplementary information
+    #
+    toplevel_result_from_storage = json.load(default_storage.open(f'{storage_prefix}/result.json', 'r'))
+    assert toplevel_result_from_storage['name'] == result_dict['name']
+    print(toplevel_result_from_storage)
+    series_0_from_storage = toplevel_result_from_storage['series']
+    assert len(series_0_from_storage) == 1
+    series_0_from_storage = series_0_from_storage[0]
+    assert series_0_from_storage['__external__'] == 'series-0.json'
+    for key, value in supplementary.items():
+        assert series_0_from_storage[key] == supplementary[key]
+    assert set(series_0_from_storage.keys()) == set(list(supplementary.keys()) + ['__external__'])
