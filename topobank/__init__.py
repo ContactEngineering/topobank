cf26f63449d070be4b45eaff2fbeb372ebebea46
Michael Röttger <roettger@tf.uni-freiburg.de>
2022-09-25 14:31:05 +0200

    MAINT: Reverted version number setting.
    Currently we don't force topobank to be installed as a package.

--- topobank/__init__.py
+++ topobank/__init__.py
@@ -1,6 +1,7 @@
-import pkg_resources
-__version__ = pkg_resources.require("topobank")[0].version
+# import pkg_resources
+# __version__ = pkg_resources.require("topobank")[0].version
 
+__version__ = "0.92.0"
 __version_info__ = tuple(
     [
         int(num) if num.isdigit() else num
