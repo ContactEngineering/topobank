4e3980339d392e6d4c6310da56196ab77ed24b1d
Michael Röttger <roettger@tf.uni-freiburg.de>
2022-09-30 14:30:59 +0200

    TST: Added test for permission check during download of analyses.

--- topobank/analysis/tests/test_downloads.py
+++ topobank/analysis/tests/test_downloads.py
@@ -1,9 +1,6 @@
 """
 Test of downloads module.
 """
-import pickle
-import zipfile
-from io import BytesIO, StringIO
 
 from django.shortcuts import reverse
 
@@ -29,4 +26,27 @@ def test_download_plot_analyses_to_txt(rf):
     assert_in_content(response, 'Fibonacci')
     assert_in_content(response, '1.000000000000000000e+00 0.000000000000000000e+00 0.000000000000000000e+00')
     assert_in_content(response, '8.000000000000000000e+00 1.300000000000000000e+01 0.000000000000000000e+00')
+
+
+@pytest.mark.parametrize("user_has_plugin", [False, True])
+@pytest.mark.django_db
+def test_download_view_permission_for_function_from_plugin(mocker,client, user_has_plugin, handle_usage_statistics):
+    """Simple test, whether analyses which should not be visible lead to an error during download.
+    """
+    func = AnalysisFunction.objects.get(name="test")
+
+    analysis = TopographyAnalysisFactory(function=func)
+
+    m = mocker.patch('topobank.analysis.models.Analysis.is_visible_for_user')
+    m.return_value = user_has_plugin
+
+    response = client.get(reverse('analysis:download',
+                          kwargs=dict(ids=str(analysis.id),
+                                      art=ART_SERIES,
+                                      file_format='txt')))
+    if user_has_plugin:
+        assert response.status_code == 200
+    else:
+        assert response.status_code == 403
+
 
