08874fda69bc537d87c58091feb451888a8d9fce
Michael Röttger <roettger@tf.uni-freiburg.de>
2022-09-30 10:57:37 +0200

    MAINT: Removed comments.

--- topobank/analysis/signals.py
+++ topobank/analysis/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
+# from topobank.organizations.signals import OrganizationFilteredSignal
+
 import logging
 
 from ..manager.utils import recursive_delete
@@ -12,3 +14,11 @@ _log = logging.getLogger(__name__)
 @receiver(pre_delete, sender=Analysis)
 def remove_storage_files(sender, instance, **kwargs):
     recursive_delete(instance.storage_prefix)
+
+# register_analysis_function_implementation = OrganizationFilteredSignal()
+
+# analysis_function_names = OrganizationFilteredSignal()
+
+
+
+
