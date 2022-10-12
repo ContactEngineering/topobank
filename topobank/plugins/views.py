38fb82a5d991095551340ca912e8a86a098420a6
Michael Röttger <roettger@tf.uni-freiburg.de>
2022-10-12 15:27:23 +0200

    MAINT: Only show plugins available to the current user.

--- topobank/plugins/views.py
+++ topobank/plugins/views.py
@@ -2,13 +2,20 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.apps import apps
 
+from topobank.organizations.models import Organization
+
+
 class PluginListView(TemplateView):
     template_name = "plugins/plugins_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context['plugin_apps'] = [ apps.get_app_config(app) for app in settings.PLUGIN_APPS]
+        # only show plugins available for his user
+        plugins_available = Organization.objects.get_plugins_available(self.request.user)
+        context['plugin_apps'] = [apps.get_app_config(app) for app in settings.PLUGIN_APPS
+                                  if app in plugins_available]
+
         #
         # Add context needed for tabs
         #
