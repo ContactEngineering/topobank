9598cd6e35d7f56d28ccfe5222e0d5916c77539c
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-03-01 10:23:09 +0100

    MAINT: PackageNotFoundError -> ModuleNotFoundError

--- topobank/__init__.py
+++ topobank/__init__.py
@@ -1,6 +1,6 @@
 try:
     from importlib.metadata import version
     __version__ = version("topobank")
-except PackageNotFoundError:
+except ModuleNotFoundError:
     from setuptools_scm import get_version
     __version__ = get_version()
