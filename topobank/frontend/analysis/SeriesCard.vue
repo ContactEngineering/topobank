368ca3178096af20da6c061a5288b14ff97dbf5a
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-03-22 16:59:57 +0100

    MAINT: `enlargeButton` -> `enlarged`

--- topobank/frontend/analysis/SeriesCard.vue
+++ topobank/frontend/analysis/SeriesCard.vue
@@ -17,9 +17,9 @@ export default {
     apiUrl: String,
     csrfToken: String,
     detailUrl: String,
-    enlargeButton: {
+    enlarged: {
       type: Boolean,
-      default: false
+      default: true
     },
     functionId: Number,
     functionName: String,
@@ -97,7 +97,8 @@ export default {
         <button @click="updateCard" class="btn btn-default float-right ml-1">
           <i class="fa fa-redo"></i>
         </button>
-        <div v-if="enlargeButton" class="btn-group btn-group-sm float-right">
+        <div v-if="!enlarged
+" class="btn-group btn-group-sm float-right">
           <a :href="detailUrl" class="btn btn-default float-right">
             <i class="fa fa-expand"></i>
           </a>
