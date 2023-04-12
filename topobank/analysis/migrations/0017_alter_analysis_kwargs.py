500e63fcaa1d1f1c61c7d945ee6eb26ddaa170fb
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-04-12 23:20:48 +0200

    Revert "MAINT: kwargs should not be able to be null"
    This reverts commit c0a1481f5bddb062c957cfbbd775c6a889749ac7.

--- topobank/analysis/migrations/0017_alter_analysis_kwargs.py
+++ topobank/analysis/migrations/0017_alter_analysis_kwargs.py
@@ -41,7 +41,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='analysis',
             name='kwargs',
-            field=models.JSONField(),
+            field=models.JSONField(null=True),
         ),
         migrations.RunPython(
             forward_func,
