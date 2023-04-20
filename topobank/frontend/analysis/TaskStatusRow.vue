f1b0720afa41d2f7e2c7785710abd6d9bc3aa949
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-04-20 07:24:56 +0200

    WIP: Error reporting and auto-update when tasks finish

--- topobank/frontend/analysis/TaskStatusRow.vue
+++ topobank/frontend/analysis/TaskStatusRow.vue
@@ -2,6 +2,9 @@
 
 export default {
   name: 'task-status-row',
+  emits: [
+    'setTaskStatus'
+  ],
   props: {
     analysis: Object,
     csrfToken: String,
@@ -12,7 +15,8 @@ export default {
   },
   data() {
     return {
-      _analysis: this.analysis
+      _analysis: this.analysis,
+      _error: null
     }
   },
   mounted() {
@@ -25,6 +29,21 @@ export default {
         if (this._analysis.task_state == 'pe' || this._analysis.task_state == 'st') {
           this.$emit('setTaskStatus', true);
           setTimeout(this.checkStatus, this.statusTimeout);
+        } else if (this._analysis.task_state == 'fa') {
+          // This is failure. Query reason.
+          console.log('failed!!');
+          fetch(`${this._analysis.api.dataUrl}/result.json`, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'X-CSRFToken': this.csrfToken
+            }
+          })
+              .then(response => response.json())
+              .then(result => {
+                console.log(result);
+              });
         } else {
           // Task seems to have finished or failed
           this.$emit('setTaskStatus', false);
@@ -35,7 +54,7 @@ export default {
       }
     },
     checkStatus() {
-      fetch(this._analysis.urls.status, {
+      fetch(this._analysis.api.statusUrl, {
         method: 'GET',
         headers: {
           'Accept': 'application/json',
@@ -50,7 +69,7 @@ export default {
           })
     },
     renew() {
-      fetch(this._analysis.urls.status, {
+      fetch(this._analysis.api.statusUrl, {
         method: 'PUT',
         headers: {
           'Accept': 'application/json',
@@ -77,10 +96,12 @@ export default {
       <div v-if="_analysis.task_state == 'fa'" class="btn btn-default bg-danger disabled">
         <i class="fa fa-circle text-white"></i>
       </div>
-      <div v-if="_analysis.task_state == 'pe' || _analysis.task_progress === null" class="btn btn-default bg-light disabled">
+      <div v-if="_analysis.task_state == 'pe' || _analysis.task_progress === null"
+           class="btn btn-default bg-light disabled">
         <div class="spinner text-white"></div>
       </div>
-      <div v-if="_analysis.task_state == 'st' && _analysis.task_progress !== null" class="btn btn-default bg-light disabled">
+      <div v-if="_analysis.task_state == 'st' && _analysis.task_progress !== null"
+           class="btn btn-default bg-light disabled">
         {{ Math.round(_analysis.task_progress.percent) }} %
       </div>
     </td>
