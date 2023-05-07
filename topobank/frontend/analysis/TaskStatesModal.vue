64ba8b56cbc34243934160e456fabd2375266886
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-05-07 13:36:19 +0200

    MAINT: Indentation

--- topobank/frontend/analysis/TaskStatesModal.vue
+++ topobank/frontend/analysis/TaskStatesModal.vue
@@ -5,91 +5,91 @@
 import TaskStateRow from "./TaskStateRow.vue";
 
 export default {
-  name: 'task-states-modal',
-  emits: [
-    'taskStateChanged'
-  ],
-  components: {
-    TaskStateRow
-  },
-  props: {
-    analyses: {
-      type: Object,
-      default: []
+    name: 'task-states-modal',
+    emits: [
+        'taskStateChanged'
+    ],
+    components: {
+        TaskStateRow
     },
-    csrfToken: String
-  },
-  data() {
-    return {
-      _analyses: this.analyses === undefined ? [] : this.analyses,
-      _taskStates: this.analyses === undefined ? [] : this.getInitialTaskStates(this.analyses),
-      _selectedAnalyses: []
-    }
-  },
-  watch: {
-    analyses: {
-      handler(newValue, oldValue) {
-        this._analyses = newValue === undefined ? [] : newValue;
-        this._taskStates = newValue === undefined ? [] : this.getInitialTaskStates(newValue);
+    props: {
+        analyses: {
+            type: Object,
+            default: []
+        },
+        csrfToken: String
+    },
+    data() {
+        return {
+            _analyses: this.analyses === undefined ? [] : this.analyses,
+            _taskStates: this.analyses === undefined ? [] : this.getInitialTaskStates(this.analyses),
+            _selectedAnalyses: []
+        }
+    },
+    watch: {
+        analyses: {
+            handler(newValue, oldValue) {
+                this._analyses = newValue === undefined ? [] : newValue;
+                this._taskStates = newValue === undefined ? [] : this.getInitialTaskStates(newValue);
+                this.$emit('taskStateChanged', this._taskStates);
+            }
+        }
+    },
+    mounted() {
         this.$emit('taskStateChanged', this._taskStates);
-      }
-    }
-  },
-  mounted() {
-    this.$emit('taskStateChanged', this._taskStates);
-  },
-  methods: {
-    getInitialTaskStates(analyses) {
-      return analyses.map(a => a.task_state);
     },
-    setTaskState(analysisIndex, taskState) {
-      this._taskStates[analysisIndex] = taskState;
-      this.$emit('taskStateChanged', this._taskStates);
+    methods: {
+        getInitialTaskStates(analyses) {
+            return analyses.map(a => a.task_state);
+        },
+        setTaskState(analysisIndex, taskState) {
+            this._taskStates[analysisIndex] = taskState;
+            this.$emit('taskStateChanged', this._taskStates);
+        }
     }
-  }
 };
 </script>
 
 <template>
-  <div class="modal fade" tabindex="-1" role="dialog"
-       aria-labelledby="taskStatesModalLabel"
-       aria-hidden="true">
-    <div class="modal-dialog modal-xl" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="taskStatesModalLabel">Tasks</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
+    <div class="modal fade" tabindex="-1" role="dialog"
+         aria-labelledby="taskStatesModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="taskStatesModalLabel">Tasks</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div v-if="_analyses.length > 0" class="modal-body">
+                    <small>
+                        <table class="table table-hover task-table">
+                            <thead>
+                            <tr>
+                                <th scope="col" style="width:100px"></th>
+                                <th scope="col">Task description</th>
+                                <th scope="col" style="width:150px">Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <task-state-row
+                                    v-for="(analysis, index) in _analyses"
+                                    :analysis="analysis"
+                                    :csrf-token="csrfToken"
+                                    @set-task-state="(taskState) => setTaskState(index, taskState)">
+                            </task-state-row>
+                            </tbody>
+                        </table>
+                    </small>
+                    <div v-if="_analyses.length == 0" class="alert alert-info">
+                        No analysis was triggered for this function and these subjects.
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button>
+                </div>
+            </div>
         </div>
-        <div v-if="_analyses.length > 0" class="modal-body">
-          <small>
-            <table class="table table-hover task-table">
-              <thead>
-              <tr>
-                <th scope="col" style="width:100px"></th>
-                <th scope="col">Task description</th>
-                <th scope="col" style="width:150px">Actions</th>
-              </tr>
-              </thead>
-              <tbody>
-                <task-state-row
-                    v-for="(analysis, index) in _analyses"
-                    :analysis="analysis"
-                    :csrf-token="csrfToken"
-                    @set-task-state="(taskState) => setTaskState(index, taskState)">
-                </task-state-row>
-              </tbody>
-            </table>
-          </small>
-          <div v-if="_analyses.length == 0" class="alert alert-info">
-            No analysis was triggered for this function and these subjects.
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
--- topobank/frontend/analysis/TasksButton.vue
+++ topobank/frontend/analysis/TasksButton.vue
@@ -6,80 +6,80 @@ import TaskStatesModal from './TaskStatesModal.vue';
 
 // Count the number of unique entries in the array `arr`.
 function countUniqueEntries(arr, requiredKeys) {
-  let nbElements = {};
-  // Count elements
-  for (let value of arr) {
-    if (nbElements[value] === undefined) {
-      nbElements[value] = 1;
-    } else {
-      nbElements[value] += 1;
+    let nbElements = {};
+    // Count elements
+    for (let value of arr) {
+        if (nbElements[value] === undefined) {
+            nbElements[value] = 1;
+        } else {
+            nbElements[value] += 1;
+        }
     }
-  }
-  // Make sure `requiredKeys` are present in the return object
-  if (requiredKeys !== undefined) {
-    for (let key of requiredKeys) {
-      if (nbElements[key] === undefined) {
-        nbElements[key] = 0;
-      }
+    // Make sure `requiredKeys` are present in the return object
+    if (requiredKeys !== undefined) {
+        for (let key of requiredKeys) {
+            if (nbElements[key] === undefined) {
+                nbElements[key] = 0;
+            }
+        }
     }
-  }
-  return nbElements;
+    return nbElements;
 }
 
 export default {
-  name: 'tasks-button',
-  emits: [
-      'taskStateChanged'
-  ],
-  components: {
-    TaskStatesModal
-  },
-  props: {
-    analyses: Object,
-    csrfToken: String,
-    uid: {
-      type: String,
-      default() {
-        return uuid4();
-      }
+    name: 'tasks-button',
+    emits: [
+        'taskStateChanged'
+    ],
+    components: {
+        TaskStatesModal
+    },
+    props: {
+        analyses: Object,
+        csrfToken: String,
+        uid: {
+            type: String,
+            default() {
+                return uuid4();
+            }
+        }
+    },
+    data() {
+        return {
+            _anyTaskIsRunning: true,  // We assume some task is running and get notified if this is not the case
+            _nbFailed: 0,
+            _nbRunningOrPending: 0,
+            _nbSuccess: 0,
+        }
+    },
+    methods: {
+        taskStateChanged(taskStates) {
+            let nbStates = countUniqueEntries(taskStates, ['pe', 'st', 're', 'fa', 'su']);
+            this._nbRunningOrPending = nbStates['pe'] + nbStates['st'] + nbStates['re'];
+            this._nbSuccess = nbStates['su'];
+            this._nbFailed = nbStates['fa'];
+            this.$emit('taskStateChanged', this._nbRunningOrPending, this._nbSuccess, this._nbFailed);
+        }
     }
-  },
-  data() {
-    return {
-      _anyTaskIsRunning: true,  // We assume some task is running and get notified if this is not the case
-      _nbFailed: 0,
-      _nbRunningOrPending: 0,
-      _nbSuccess: 0,
-    }
-  },
-  methods: {
-    taskStateChanged(taskStates) {
-      let nbStates = countUniqueEntries(taskStates, ['pe', 'st', 're', 'fa', 'su']);
-      this._nbRunningOrPending = nbStates['pe'] + nbStates['st'] + nbStates['re'];
-      this._nbSuccess = nbStates['su'];
-      this._nbFailed = nbStates['fa'];
-      this.$emit('taskStateChanged', this._nbRunningOrPending, this._nbSuccess, this._nbFailed);
-    }
-  }
 }
 
 </script>
 
 <template>
-  <button class="btn btn-default btn-sm float-right"
-          href="#"
-          data-toggle="modal"
-          :data-target="`#task-states-modal-${uid}`">
-    <span v-if="_nbRunningOrPending > 0" class="spinner"></span>
-      Tasks
-    <span v-if="_nbRunningOrPending > 0" class="badge badge-secondary ml-1">{{ _nbRunningOrPending }}</span>
-    <span v-if="_nbSuccess > 0" class="badge badge-success ml-1">{{ _nbSuccess }}</span>
-    <span v-if="_nbFailed > 0" class="badge badge-danger ml-1">{{ _nbFailed }}</span>
-  </button>
-  <task-states-modal
-      :id="`task-states-modal-${uid}`"
-      :analyses="analyses"
-      :csrf-token="csrfToken"
-      @task-state-changed="taskStateChanged">
-  </task-states-modal>
+    <button class="btn btn-default btn-sm float-right"
+            href="#"
+            data-toggle="modal"
+            :data-target="`#task-states-modal-${uid}`">
+        <span v-if="_nbRunningOrPending > 0" class="spinner"></span>
+        Tasks
+        <span v-if="_nbRunningOrPending > 0" class="badge badge-secondary ml-1">{{ _nbRunningOrPending }}</span>
+        <span v-if="_nbSuccess > 0" class="badge badge-success ml-1">{{ _nbSuccess }}</span>
+        <span v-if="_nbFailed > 0" class="badge badge-danger ml-1">{{ _nbFailed }}</span>
+    </button>
+    <task-states-modal
+            :id="`task-states-modal-${uid}`"
+            :analyses="analyses"
+            :csrf-token="csrfToken"
+            @task-state-changed="taskStateChanged">
+    </task-states-modal>
 </template>
