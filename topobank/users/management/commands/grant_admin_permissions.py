d02271a81992eee31742868b020589fbee1054b3
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-07-25 08:36:02 +0200

    BUG: Need to use natural key for permission

--- topobank/users/management/commands/grant_admin_permissions.py
+++ topobank/users/management/commands/grant_admin_permissions.py
@@ -1,10 +1,12 @@
+import sys
+import logging
+
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 # from guardian.shortcuts import get_user_perms, remove_perm
 
-import sys
-import logging
+from request_profiler.models import ProfilingRecord, RuleSet
 
 from topobank.organizations.models import Organization
 from topobank.users.models import User
@@ -19,6 +21,8 @@ PERMISSIONS_TO_GRANT = {  # key: model, value: list of actions - subset of {"del
     User: ['view'],
     Group: ['add', 'view', 'change', 'delete'],
     Organization: ['add', 'view', 'change', 'delete'],
+    ProfilingRecord: ['add', 'view', 'change', 'delete'],
+    RuleSet: ['add', 'view', 'change', 'delete'],
 }
 
 
@@ -38,11 +42,12 @@ class Command(BaseCommand):
             sys.exit(1)
 
         #
-        # Ensure user has "staff" flag
+        # Ensure user has "staff" flag but not the "superuser" flag
         #
         user.is_staff = True
+        user.is_superuser = False
         user.save()
-        _log.info("User is staff member now.")
+        _log.info("User is `staff` member now, but not `superuser`.")
 
         #
         # Ensure user has permissions needed
@@ -50,8 +55,10 @@ class Command(BaseCommand):
         for model, perms in PERMISSIONS_TO_GRANT.items():
             ct = ContentType.objects.get_for_model(model)
             for perm in perms:
-                perm = Permission.objects.get(codename=f"{perm}_{ct.name}", content_type=ct)
-                _log.info(f"Granting permission {perm}..")
+                module_name, model_name = ct.natural_key()
+                _log.info(f"Obtaining permission object for '{perm}_{model_name}'...")
+                perm = Permission.objects.get(codename=f"{perm}_{model_name}", content_type=ct)
+                _log.info(f"Granting permission {perm}...")
                 user.user_permissions.add(perm)
 
         self.stdout.write(self.style.SUCCESS(
