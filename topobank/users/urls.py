1997d302198afd96aba0ebda3f823c1a4218c85c
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-09-03 15:13:22 +0200

    ENH: API access to user information

--- topobank/users/urls.py
+++ topobank/users/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
+from rest_framework.routers import DefaultRouter
+
 from . import views
 
+router = DefaultRouter()
+router.register(r'api/user', views.UserViewSet, basename='user-api')
+
+urlpatterns = router.urls
+
 app_name = "users"
-urlpatterns = [
+urlpatterns += [
     # path("", view=views.UserListView.as_view(), name="list"),
     path("~redirect/", view=views.UserRedirectView.as_view(), name="redirect"),
     path("~update/", view=views.UserUpdateView.as_view(), name="update"),
