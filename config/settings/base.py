0732a08b98815ffa74fb6ef5002aa900af22d561
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-11-06 13:01:54 +0100

    MAINT: Bootstrap 5 crispy forms

--- config/settings/base.py
+++ config/settings/base.py
@@ -242,7 +242,8 @@ TEMPLATES = [
     },
 ]
 # http://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # FIXTURES
 # ------------------------------------------------------------------------------
