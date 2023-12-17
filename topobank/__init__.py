e2d33e8f8e43d7b24ce2cf38e8a1f93d48f5ba0e
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-12-17 23:30:43 +0100

    MAINT: `importlib.metadata` exists because we require Python 3.8

--- topobank/__init__.py
+++ topobank/__init__.py
@@ -1,6 +1,2 @@
-try:
-    from importlib.metadata import version
-    __version__ = version("topobank")
-except ModuleNotFoundError:
-    from setuptools_scm import get_version
-    __version__ = get_version()
+from importlib.metadata import version
+__version__ = version("topobank")
