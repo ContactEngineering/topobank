249e2134207955df0a3d71fa0bc37476ddc7f9e3
Lars Pastewka <lars.pastewka@surface.design>
2024-08-01 22:53:55 +0200

    TST: Added test for triggering analyses when sharing topographies

--- topobank/analysis/tests/test_sharing.py
+++ topobank/analysis/tests/test_sharing.py
@@ -1,11 +1,51 @@
 import pytest
 
 from topobank.analysis.controller import AnalysisController
-from topobank.manager.models import Surface
+from topobank.manager.models import Surface, Topography
 
 
 @pytest.mark.django_db
 def test_topography_analysis(mocker, two_users, test_analysis_function):
+    user1, user2 = two_users
+
+    surface1, surface2, surface3 = Surface.objects.all()
+    topography1, topography2, topography3 = Topography.objects.all()
+
+    controller = AnalysisController(
+        user1, subjects=[topography1], function=test_analysis_function
+    )
+    controller.trigger_missing_analyses()
+    assert len(controller) == 1  # analysis of topography
+
+    results = controller.get()
+
+    for r in results:
+        r.task_state = "su"
+
+    # Controller should not trigger analyses again
+    controller.trigger_missing_analyses()
+    assert [r.task_state for r in controller.get()] == ["su"]
+
+    controller = AnalysisController(
+        user2, subjects=[topography1], function=test_analysis_function
+    )
+    controller.trigger_missing_analyses()
+    assert len(controller) == 0  # user2 has no access to surface1
+
+    surface1.share(user2)
+
+    controller = AnalysisController(
+        user2, subjects=[topography1], function=test_analysis_function
+    )
+    controller.trigger_missing_analyses()
+    assert len(controller) == 1  # user2 now has access to topography1
+
+    # Controller triggered analyses because they are tied to a specific user
+    assert [r.task_state for r in controller.get()] == ["pe"]
+
+
+@pytest.mark.django_db
+def test_surface_analysis(mocker, two_users, test_analysis_function):
     user1, user2 = two_users
 
     surface1, surface2, surface3 = Surface.objects.all()
