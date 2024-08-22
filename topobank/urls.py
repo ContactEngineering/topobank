5e3390d25510725ee92b0e426e6ad9ab96001460
Ioannis Nezis <ioannis@nezis.de>
2024-08-22 16:18:18 +0200

    FEAT: add drf-spectacular for OpenApi support

--- topobank/urls.py
+++ topobank/urls.py
@@ -8,6 +8,7 @@ from django.contrib import admin
 from django.contrib.auth.decorators import user_passes_test
 from django.urls import include, path, re_path
 from django.views import defaults as default_views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from topobank.organizations.models import Organization
 from topobank.users.allauth_views import TabbedEmailView
@@ -20,8 +21,9 @@ urlpatterns = [
         "users/",
         include("topobank.users.urls", namespace="users"),
     ),
-    re_path("^accounts/email/$", TabbedEmailView.as_view(),
-            name='account_email'),  # same as allauth.accounts.email.EmailView, but with tab data
+    re_path(
+        "^accounts/email/$", TabbedEmailView.as_view(), name="account_email"
+    ),  # same as allauth.accounts.email.EmailView, but with tab data
     path("accounts/", include("allauth.urls")),
     #
     # Core topobank applications
@@ -45,13 +47,21 @@ urlpatterns = [
     #
     # Notifications - see package django-notifications
     #
-    re_path('^inbox/notifications/', include(notifications.urls, namespace='notifications')),
+    re_path(
+        "^inbox/notifications/", include(notifications.urls, namespace="notifications")
+    ),
     #
     # Watchman - see package django-watchman
     #
+    path("watchman/", include(("watchman.urls", "watchman"), namespace="watchman")),
+    #
+    # Open Api
+    #
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "watchman/",
-        include(("watchman.urls", "watchman"), namespace="watchman")
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
 ]
 
@@ -92,33 +102,37 @@ if settings.DEBUG:
 #
 def plugin_urls(urllist, app):
     for entry in urllist:
-        if hasattr(entry, 'url_patterns'):
+        if hasattr(entry, "url_patterns"):
             # This is a list of URL patterns
             entry.url_patterns = plugin_urls(entry.url_patterns, app)
-        elif hasattr(entry, 'callback'):
+        elif hasattr(entry, "callback"):
             # This is a path with a view
             def plugin_available_check(user):
                 if app.TopobankPluginMeta.restricted:
                     return app.name in Organization.objects.get_plugins_available(user)
                 return True
 
-            callback_decorator = user_passes_test(plugin_available_check, login_url='/403/', redirect_field_name=None)
+            callback_decorator = user_passes_test(
+                plugin_available_check, login_url="/403/", redirect_field_name=None
+            )
             entry.callback = callback_decorator(entry.callback)
     return urllist
 
 
 plugin_patterns = []
 for app in apps.get_app_configs():
-    if hasattr(app, 'TopobankPluginMeta'):
-        url_module_name = app.name + '.urls'
+    if hasattr(app, "TopobankPluginMeta"):
+        url_module_name = app.name + ".urls"
         if importlib.util.find_spec(url_module_name):
             url_module = importlib.import_module(url_module_name)
-            if hasattr(url_module, 'urlprefix') and url_module.urlprefix is not None:
+            if hasattr(url_module, "urlprefix") and url_module.urlprefix is not None:
                 plugin_patterns.append(
                     path(
                         url_module.urlprefix,  # all urls of this plugin start with this
                         # Allow access only if plugin available
-                        include((plugin_urls(url_module.urlpatterns, app=app), app.label))
+                        include(
+                            (plugin_urls(url_module.urlpatterns, app=app), app.label)
+                        ),
                         # Allow access independent of plugin availability (always permitted)
                         # include((url_module.urlpatterns, app.label))
                     )
