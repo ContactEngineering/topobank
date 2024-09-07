a899c2beea4dbc95e5ef4f92db1260c6c9fd9f5f
Lars Pastewka <lars.pastewka@surface.design>
2024-09-07 09:07:09 +0200

    ENH: Return dictionary for easy filename lookup when listing manifests

--- topobank/files/views.py
+++ topobank/files/views.py
@@ -88,7 +88,7 @@ def list_manifests(request, pk=None):
     obj = get_object_or_404(Folder, pk=pk)
     if not obj.has_permission(request.user, "view"):
         return HttpResponseForbidden()
-    return [
-        ManifestSerializer(manifest, context={"request": request}).data
+    return {
+        obj.filename: ManifestSerializer(manifest, context={"request": request}).data
         for manifest in obj.files
-    ]
+    }
