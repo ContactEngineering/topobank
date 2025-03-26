c5b62f9ce2481dad0808e9e07f0349d8588fb2fb
Quang-Thuy Hoang <qnh@Mac.attlocal.net>
2025-03-26 15:53:39 -0500

    add date formatter

--- topobank/files/utils.py
+++ topobank/files/utils.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+
+
 def file_storage_path(instance, filename: str) -> str:
     if not instance.pk:
         raise RuntimeError(
             f"Cannot construct storage path from unsaved instance {instance}."
         )
     return f"data-lake/{instance.id}/{filename}"
+
+
+def format_datetime(dt: datetime) -> (str, str):
+    date_str = dt.strftime('%Y-%m-%d')
+    time_utc_str = dt.utcnow().strftime('%H:%M:%S')
+    return date_str, time_utc_str
