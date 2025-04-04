6444a7767968eb6fec46654f33e275c2895b08e1
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-04-04 09:00:56 +0200

    MAINT: Removed unused import

--- topobank/users/adapters.py
+++ topobank/users/adapters.py
@@ -1,6 +1,5 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.urls import reverse
 
