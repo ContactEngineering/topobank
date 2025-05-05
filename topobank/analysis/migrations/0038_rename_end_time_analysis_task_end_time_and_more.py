a2903e1c67cdd5a6f651aec76360877eef74dded
Lars Pastewka <lars.pastewka@surface.design>
2025-05-05 16:10:34 +0200

    MAINT: Fixed conflicting migrations

--- topobank/analysis/migrations/0038_rename_end_time_analysis_task_end_time_and_more.py
+++ topobank/analysis/migrations/0038_rename_end_time_analysis_task_end_time_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('analysis', '0036_analysis_description'),
+        ('analysis', '0037_workflowtemplate'),
     ]
 
     operations = [
