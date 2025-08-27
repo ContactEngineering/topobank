07a77876a1610885b82adb75cc33d34deac24296
Lars Pastewka <lars.pastewka@surface.design>
2025-08-27 17:20:29 +0200

    ENH: User and organization serializers return auxiliary API endpoints

--- topobank/organizations/serializers.py
+++ topobank/organizations/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from ..supplib.serializers import StrictFieldMixin
 from .models import Organization
@@ -7,8 +8,32 @@ from .models import Organization
 class OrganizationSerializer(StrictFieldMixin, serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Organization
-        fields = ["url", "id", "name", "plugins_available"]
+        fields = [
+            # Self
+            "url",
+            "id",
+            # Auxiliary API endpoints
+            'api',
+            # Model fields
+            "name",
+            "plugins_available"
+        ]
 
     url = serializers.HyperlinkedIdentityField(
         view_name="organizations:organization-v1-detail", read_only=True
     )
+    api = serializers.SerializerMethodField()
+
+    def get_api(self, obj: Organization) -> dict:
+        return {
+            "add_user": reverse(
+                "organizations:add-user-v1",
+                kwargs={"pk": obj.id},
+                request=self.context["request"],
+            ),
+            "remove_user": reverse(
+                "organizations:remove-user-v1",
+                kwargs={"pk": obj.id},
+                request=self.context["request"],
+            )
+        }
