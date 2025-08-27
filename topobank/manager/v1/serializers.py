18d546fcc692689a15c102a29b5a06a05a98315a
Lars Pastewka <lars.pastewka@surface.design>
2025-08-27 12:52:59 +0200

    MAINT: v2 Surface serializer, refactored PropertyField into properties app

--- topobank/manager/v1/serializers.py
+++ topobank/manager/v1/serializers.py
@@ -1,16 +1,15 @@
 import logging
 
 import pydantic
-from django.db import transaction
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from tagulous.contrib.drf import TagRelatedManagerField
 
-from topobank.files.serializers import ManifestSerializer
-from topobank.manager.models import Surface, Tag, Topography
-from topobank.properties.models import Property
-from topobank.supplib.serializers import StrictFieldMixin
-from topobank.taskapp.serializers import TaskStateModelSerializer
+from ...files.serializers import ManifestSerializer
+from ...manager.models import Surface, Tag, Topography
+from ...properties.serializers import PropertiesField
+from ...supplib.serializers import StrictFieldMixin
+from ...taskapp.serializers import TaskStateModelSerializer
 
 _log = logging.getLogger(__name__)
 
@@ -247,57 +246,7 @@ class TopographySerializer(StrictFieldMixin, TaskStateModelSerializer):
             raise serializers.ValidationError({"message": str(exc)})
 
 
-class ValueField(serializers.Field):
-    def to_representation(self, value):
-        return value
 
-    def to_internal_value(self, data):
-        return data
-
-
-class PropertiesField(serializers.Field):
-    def to_representation(self, value):
-        ret = {}
-        for prop in value.all():
-            ret[prop.name] = {"value": prop.value}
-            if prop.unit is not None:
-                ret[prop.name]["unit"] = str(prop.unit)
-        return ret
-
-    def to_internal_value(self, data: dict[str, dict[str, str]]):
-        surface: Surface = self.root.instance
-        with (
-            transaction.atomic()
-        ):  # NOTE: This is probably not needed because django wraps views in a transaction.
-            # WARNING: with the current API design surfaces can only be created with no properties.
-            if surface is not None:
-                surface.properties.all().delete()
-                for property in data:
-                    # NOTE: Validate that a numeric value has a unit
-                    if (
-                        isinstance(data[property]["value"], (int, float))
-                        and "unit" not in data[property]
-                    ):
-                        raise serializers.ValidationError(
-                            {property: "numeric properties must have a unit"}
-                        )
-                    elif (
-                        isinstance(data[property]["value"], str)
-                        and "unit" in data[property]
-                    ):
-                        raise serializers.ValidationError(
-                            {property: "categorical properties must not have a unit"}
-                        )
-
-                    Property.objects.create(
-                        surface=surface,
-                        name=property,
-                        value=data[property]["value"],
-                        unit=data[property].get("unit"),
-                    )
-
-                return self.root.instance.properties.all()
-        return []
 
 
 class SurfaceSerializer(StrictFieldMixin, serializers.HyperlinkedModelSerializer):
