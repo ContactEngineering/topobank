65a4754e34edf0176c709feb4094ac6f9551e76d
Lars Pastewka <lars.pastewka@surface.design>
2025-08-28 22:26:11 -0400

    MAINT: Reverted database configuration

--- topobank/settings/base.py
+++ topobank/settings/base.py
@@ -49,7 +49,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 postgres_db = env("POSTGRES_DB", default=None)
 if postgres_db is None:
-    DATABASES = {"default": env.db("DATABASE_URL", default="postgres://postgres@localhost/topobank-test")}
+    DATABASES = {"default": env.db("DATABASE_URL", default="postgres:///topobank-test")}
 else:
     DATABASES = {
         "default": {
