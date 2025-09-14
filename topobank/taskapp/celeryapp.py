4c9a73b6b52e534e6df516662acf0b3d42de9091
Lars Pastewka <lars.pastewka@surface.design>
2025-09-14 20:28:06 +0200

    MAINT: Disabled Celery beat for `save_landing_page_statistics`

--- topobank/taskapp/celeryapp.py
+++ topobank/taskapp/celeryapp.py
@@ -29,9 +29,9 @@ class CeleryAppConfig(AppConfig):
         # Therefore I'm using an explicit configuration of the schedule instead
         # of using the decorator.
         #
-        app.conf.beat_schedule = {
-            'save-landing-page-statistics': {
-                'task': 'topobank.taskapp.tasks.save_landing_page_statistics',
-                'schedule': crontab(hour='0', minute='0'),
-            }
-        }
+        # app.conf.beat_schedule = {
+        #     'save-landing-page-statistics': {
+        #         'task': 'topobank.taskapp.tasks.save_landing_page_statistics',
+        #         'schedule': crontab(hour='0', minute='0'),
+        #     }
+        # }
